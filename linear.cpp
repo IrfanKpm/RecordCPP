053fd0a25a612206480236dd8c3186608ef3f76d
IrfanKpm <112098500+IrfanKpm@users.noreply.github.com>
2023-02-02 07:18:41 +0530

    Update linear.cpp

--- linear.cpp
+++ linear.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main(){
-	int arr[20];
-	int i,n,x;
-	
-	cout<<"How many elements :: ";
-	cin>>n;
-	for(i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	cout<<"Enter Elemet to search :: ";
-	cin >> x;
-	
-	for(i=0;i<n;i++){
-	  if(arr[i]==x){
-		 cout<<"Element Found at Index > "<<i;
-		 return 0;
-	  }
-	}
-	cout<<"Element not Found";	
+    int i,x,n;
+    cout<<"Enter Number of elements in array : ";
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cout<<"Enter "<<i<<"th element : ";
+        cin>>arr[i];
+    }
+    cout<<"Enter element to search : ";
+    cin>>x;
+    for(i=0;i<n;i++){
+        if(arr[i]==x){
+            cout<<"Element ound at index : "<<i;
+            return 0;
+        }
+    }
+    cout<<"Element not fount in array";
+    return 0;
 }
